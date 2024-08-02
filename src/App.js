8d6b2c7746c5335bf5c1041dab294a7a18767cca
ashif51 <ashif51h@gmail.com>
2024-08-03 00:50:55 +0530

    Nav bar and links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
-import './App.css';
-import {Butto, Button} from 'react-bootstrap'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Login from "./Login";
+import Register from "./Register";
+import AddProduct from "./AddProduct";
+import UpdateProduct from "./UpdateProduct";
 
 function App() {
   return (
     <div className="App">
-      <h1> Ecom Project </h1>
-      <button>Normal Button</button>
-      <Button>Bootstrap Button</Button>
-
+      <BrowserRouter>
+        <Header />
+        <h1> Ecom Project </h1>
+        <Routes>
+        <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/update" element={<UpdateProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
